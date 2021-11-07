1015b16f6dbb3c4d5d3fde095db65ed284ec121f
Bilgihan Kose <bilgihankose@gmail.com>
2021-11-07 11:29:08 +0300

    fixed routes

---
+++ routes.js
@@ -0,0 +1,7 @@
+import HelloWorld from "./src/components/HelloWorld";
+import Bilgihan from "./src/components/Bilgihan";
+
+export const routes = [
+    { path:"/", component: HelloWorld },
+    { path:"/bilgihan", component: Bilgihan }
+]
---
+++ src/components/Bilgihan.vue
@@ -0,0 +1,13 @@
+<template>
+<p>Bilgihan</p>
+</template>
+
+<script>
+export default {
+  name: "Bilgihan"
+}
+</script>
+
+<style scoped>
+
+</style>
